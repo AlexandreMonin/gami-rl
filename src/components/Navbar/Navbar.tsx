76b7de44e73c357349e59d15d168197aace8541c
Alexandre Monin <alexandre.monin03@gmail.com>
2024-04-30 19:00:12 +0200

    Create navbar base

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import {JSX} from "react";
+import style from "./style.module.css";
+
+export default function Navbar() : JSX.Element {
+    return (
+      <div className={style.navbar} >
+
+      </div>
+    );
+}
