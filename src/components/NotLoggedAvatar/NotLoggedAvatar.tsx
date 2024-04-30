c036bbd5b5be229887952f64c3b66e69125daa35
Alexandre Monin <alexandre.monin03@gmail.com>
2024-04-30 20:27:40 +0200

    Add Signup page

--- src/components/NotLoggedAvatar/NotLoggedAvatar.tsx
+++ src/components/NotLoggedAvatar/NotLoggedAvatar.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 
 export default function NotLoggedAvatar(): JSX.Element {
     return (
-        <Image src="/assets/not_logged_avatar.svg" alt="Avatar de compte déconnecté" width={40} height={100} />
+        <a href="/User/Signup">
+            <Image src="/assets/not_logged_avatar.svg" alt="Avatar de compte déconnecté" width={40} height={100} />
+        </a>
     );
 }
