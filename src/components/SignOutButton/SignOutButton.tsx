92356b59491456ac3f5a75959518b42428a24165
Alexandre Monin <alexandre.monin03@gmail.com>
2024-05-01 23:12:18 +0200

    Add signout

--- src/components/SignOutButton/SignOutButton.tsx
+++ src/components/SignOutButton/SignOutButton.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import {signOut} from "next-auth/react";
 
 const SignOut = async () => {
-    const data = await signOut({ redirect: true, callbackUrl: '/' });
+    await signOut({ redirect: true, callbackUrl: '/' });
 }
 
 export default function SignOutButton() : JSX.Element {
