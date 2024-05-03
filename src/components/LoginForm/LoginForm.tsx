202f2f556581985b0ef2d1c19cb514d0bba1e777
Alexandre Monin <alexandre.monin03@gmail.com>
2024-05-03 18:14:05 +0200

    basics of the loginform component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,8 +1,31 @@
 "use client"
-export default function LoginForm() {
-    return(
-        <div>
+import {signIn} from "next-auth/react";
+import {JSX, useState} from "react";
+import style from "@/components/RegisterForm/style.module.css";
 
-        </div>
+export default function LoginForm() : JSX.Element {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signin = async () => {
+        await signIn("credentials", {
+            username: email,
+            password: password,
+            callbackUrl: '/'
+        });
+    }
+
+    return(
+        <form action={signin} method="POST">
+            <input name="email" id="email" type="email" placeholder="Email" className={style.userInput} value={email}
+                   onChange={event => setEmail(event.target.value)}/>
+            <input name="password" id="password" type="password" placeholder="Mot de passe"
+                   className={style.userInput} value={password}
+                   onChange={event => setPassword(event.target.value)}/>
+
+            <button type="submit">
+                Connexion
+            </button>
+        </form>
     )
 }
