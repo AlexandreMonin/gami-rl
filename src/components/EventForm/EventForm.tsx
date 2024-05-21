63068935c56803c7a909c415bcb942623efc2956
Alexandre Monin <alexandre.monin03@gmail.com>
2024-05-21 16:22:38 +0200

    Event form

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -9,17 +9,36 @@ export default function EventForm() {
     return (
         <form className={style.form}>
             <div className={style.checkboxGroup}>
-                <input type="checkbox" id="isPrivate" name="isPrivate" onChange={(event) => setIsPrivate(event.target.checked)}/>
+                <input type="checkbox" id="isPrivate" name="isPrivate"
+                       onChange={(event) => setIsPrivate(event.target.checked)}/>
                 <label htmlFor="isPrivate">Evènement privé</label>
             </div>
 
             <div className={style.inputGroup}>
-                <label htmlFor="name" className={style.labelInput}>Email <span
+                <label htmlFor="name" className={style.labelInput}>Nom de l&apos;évènement <span
                     className={style.required}>*</span></label>
                 <input name="email" id="email" type="email" placeholder="Mon évènement" className={style.userInput}
                        value={name}
                        onChange={event => setName(event.target.value)} required/>
             </div>
+
+            <div className={style.dateGroup}>
+                <div className={style.dateInput}>
+                    <label htmlFor="start_date">Début de l&apos;évènement</label>
+                    <input
+                        type="datetime-local"
+                        id="start_date"
+                        name="start_date"/>
+                </div>
+                <div className={style.dateInput}>
+                    <label htmlFor="end_date">Fin de l&apos;évènement</label>
+                    <input
+                        type="datetime-local"
+                        id="end_date"
+                        name="end_date"/>
+                </div>
+            </div>
+
         </form>
     )
 }
