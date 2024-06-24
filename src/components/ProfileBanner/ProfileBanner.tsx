8c9acb840f05e45093d2204d364a538c0fed1c78
Alice <a.aliceabraham@gmail.com>
2024-06-24 11:48:55 +0200

    wip : profile banner layout

--- src/components/ProfileBanner/ProfileBanner.tsx
+++ src/components/ProfileBanner/ProfileBanner.tsx
@@ -6,36 +6,39 @@ export default async function ProfileBanner({player}: { player: User }) {
 
     return (
         <div>
-            <div className={style.banner_image}>
-            </div>
-            <div className={style.row_container}>
-                <div className={style.circle}>
-                    <span>Profile Picture</span>
+            <div className={style.banner_container}>
+                <div className={style.banner_image}>
                 </div>
-                <div className={style.banner_content}>
-                    <div className={style.row_top_banner_container}>
-                        <div className={style.profile_name}>
-                            <h1 >{player.username}</h1>
+                <div className={style.row_container}>
+                    <div className={style.circle}>
+                        <span>Profile Picture</span>
+                    </div>
+                    <div className={style.banner_content}>
+                        <div className={style.row_top_banner_container}>
+                            <div className={style.profile_name}>
+                                <h1 >{player.username}</h1>
+                            </div>
+                            <div className={style.profile_platform_icons}>
+                                {/*<span>GamePlatforms</span>*/}
+                                <span className={style.dot}></span>
+                                <span className={style.dot}></span>
+                                <span className={style.dot}></span>
+                                <span className={style.dot}></span>
+                            </div>
                         </div>
-                        <div className={style.profile_platform_icons}>
-                            {/*<span>GamePlatforms</span>*/}
-                            <span className={style.dot}></span>
-                            <span className={style.dot}></span>
-                            <span className={style.dot}></span>
-                            <span className={style.dot}></span>
+                        <div className={style.row_bottom_banner_container}>
+                        <div className={style.profile_status}>
+                            <p>{player.status}</p>
+                        </div>
+                        <div className={style.profile_settings_buttons}>
+                            <span className={style.buttons}></span>
+                            <span className={style.buttons}></span>
+                            <span className={style.buttons}></span>
                         </div>
                     </div>
-                    <div className={style.row_bottom_banner_container}>
-                    <div className={style.profile_status}>
-                        <p>{player.status}</p>
-                    </div>
-                    <div className={style.profile_settings_buttons}>
-                        <span className={style.buttons}></span>
-                        <span className={style.buttons}></span>
-                        <span className={style.buttons}></span>
                     </div>
                 </div>
-                </div>
+
             </div>
             <div className={style.profile_biography}>
                 <span>{player.biography}</span>
--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -8,7 +8,7 @@ export default async function ProfilePage({id}: { id: number }) {
     const playerId = await GetPlayerById(id);
     const player = playerId.data as User;
     return (
-        <div>
+        <div className={style.profile_page}>
         {/*// <div className={style.player_card}>*/}
         {/*    <h1 className={style.player_card_name}>{player.username}</h1>*/}
         {/*    <p className={style.player_card_status}>{player.status}</p>*/}
@@ -17,15 +17,15 @@ export default async function ProfilePage({id}: { id: number }) {
         {/*    <div className={style.player_card_biography}><span>{player.biography}</span></div>*/}
             <ProfileBanner player={player}/>
             <div>
-            <div><span>FavouriteGames</span></div>
-            <div><span>PlayerLinks</span></div>
+                <div><span>FavouriteGames</span></div>
+                <div><span>PlayerLinks</span></div>
             </div>
 
-            <h3>Mes autres jeux</h3>
-            <div>
-                <h3>ProfileGamesList</h3>
-                <span>ProfileGames</span>
-            </div>
+            {/*<h3>Mes autres jeux</h3>*/}
+            {/*<div>*/}
+            {/*    <h3>ProfileGamesList</h3>*/}
+            {/*    <span>ProfileGames</span>*/}
+            {/*</div>*/}
         </div>
     )
 }
