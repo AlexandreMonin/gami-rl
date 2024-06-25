73d636ca62251a05c35baed10a77e62c77539b2f
Alice <a.aliceabraham@gmail.com>
2024-06-25 15:22:46 +0200

    user put route added

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -21,3 +21,29 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
         return NextResponse.json({ error: "An error occurred" }, { status: 500 });
     }
 }
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    const id = params.id;
+    const { status, biography } = await req.json();
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: Number(id) },
+        });
+        if (user) {
+            const updatedUser = await prisma.user.update({
+            where: { id: Number(id) },
+            data: {
+                status,
+                biography,
+            },
+            });
+            return NextResponse.json({ data: updatedUser }, { status: 200 });
+        }
+        else {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+    } catch(e) {
+        return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+    }
+}
