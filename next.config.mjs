65186e592a7de0036ffa5a54a6f8e2e5d9404586
Alexandre <alexandre.monin03@gmail.com>
2024-06-26 11:16:58 +0200

    add config for manifest

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+const PWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default PWA(nextConfig);
